e51a61a58dd41b7b23f6433409f03978f0ca61d6
trentwiles <wilest7@gmail.com>
2024-09-17 11:49:37 -0400

    cleanup code

--- menu.js
+++ menu.js
@@ -5,7 +5,7 @@ const CSV_PATH = "data/index.csv";
 
 // if it doesn't exit, then create the header of the CSV
 if (!fs.existsSync(CSV_PATH)) {
-  fs.writeFileSync(filePath, "store,price,address,url,geoJSON\n", {
+  fs.writeFileSync(CSV_PATH, "store,price,address,url,geoJSON\n", {
     flag: "w",
   });
   console.log("index.csv has been created.");
@@ -65,42 +65,38 @@ function parseMenuData(menuData, metaData) {
   console.log(price);
 
   // Format: store,price,address,url,geoJSON
-  fs.writeFile(
-    CSV_PATH,
-    `12345,${price},${metaData["price"]},${metaData["url"]},${metaData["geoJSON"]}\n`,
-    { flag: "w" },
-    (err) => {
-      if (err) {
-        console.error("Error writing to CSV file", err);
-      }
+  res = `${metaData["storeID"]},${price},${metaData["price"]},${metaData["url"]},${metaData["lat"]},${metaData["lon"]}\n`
+  return res
+}
+
+async function scrapeMenu(storeID, storeMetaData) {
+    setTimeout(() => {
+        // Code to be executed after the delay
+        console.log('Waited 2 seconds');
+      }, 2000);
+
+    // Validate arguments before anything
+    if (!storeMetaData || !storeMetaData.hasOwnProperty("address")) {
+      console.log("Invalid arguments passed for store:", storeID);
+      return false;
     }
-  );
-}
-
-function scrapeMenu(storeID, storeMetaData) {
-  // before anything, ensure that the storeMetaData looks right
-  if(storeMetaData == null || !storeMetaData.hasOwnProperty("address")){
-    console.log("invalid arguments passed")
-    return false
+  
+    const ENDPOINT = `https://mapi-dun.prod.ddmprod.dunkindonuts.com/menu-management/v1/menus/v4/${storeID}`;
+    const CONFIGURATION = {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+  
+    try {
+      const response = await axios.get(ENDPOINT, CONFIGURATION);
+      // parseMenuData is sync :)
+      await parseMenuData(response.data, storeMetaData);
+    } catch (error) {
+      console.error("Error scraping menu for store", storeID, error);
+    }
   }
-
-  const ENDPOINT = `https://mapi-dun.prod.ddmprod.dunkindonuts.com/menu-management/v1/menus/v4/${storeID}`;
-
-  const CONFIGURATION = {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  };
-
-  axios
-    .get(ENDPOINT, CONFIGURATION)
-    .then((response) => {
-      return parseMenuData(response.data, storeMetaData);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
+  
 
 module.exports = {
   scrapeMenu,
--- tester.js
+++ tester.js
@@ -1,11 +1,56 @@
-const menu = require('./menu')
-const locations = require('./locations')
+const menu = require("./menu");
+const locations = require("./locations");
+const fs = require("fs");
+const readline = require("readline");
 
+// first- add the stores and their metadata to a csv file
 locations.readFile((error, storesList) => {
-    if (error) {
-      console.error("Error reading file:", error);
-    } else {
-      console.log("Stores List:");
-      console.log(storesList);
-    }
-  });
+  if (error) {
+    console.error("Error reading file:", error);
+  } else {
+    // now we have the prepared stores list:
+    csvContent = storesList
+      .map((element) => {
+        return `${element["id"]},"${element["address"]}",${element["html_url"]},${element["geoJSON"]["coordinates"][0]},${element["geoJSON"]["coordinates"][1]}\n`;
+      })
+      .join("");
+    console.log(csvContent);
+    header = "id,address,url,lat,lon\n";
+    fs.writeFile("data/stores.csv", header + csvContent);
+  }
+});
+
+// next, read that csv file
+
+var storeMetaHolder
+const fileStream = fs.createReadStream("data/stores.csv", "utf8");
+
+// Create an interface for reading lines
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity, // Recognize all instances of CR LF (\r\n) and LF (\n) as a single line break
+});
+
+// Listen for each line
+rl.on("line", (line) => {
+  // 0 - id
+  // 1 - address
+  // 2 - url
+  // 3 - latitude
+  // 4 - longitude
+  data = line.split(",")
+  id = data[0]
+  metadata = {
+    
+  }
+});
+
+// Handle end of file
+rl.on("close", () => {
+  console.log("File reading finished.");
+});
+
+// Handle errors
+rl.on("error", (err) => {
+  console.error("Error reading file:", err);
+});
